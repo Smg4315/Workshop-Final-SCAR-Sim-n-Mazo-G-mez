88f715829a322d5ce63b51183899f42bc725bd38
Saimon <smazog@eafit.edu.co>
2025-08-09 03:17:53 -0500

    pointers optimization

--- mmm_implementation.c
+++ mmm_implementation.c
@@ -7,43 +7,52 @@
 #define MAX_SIZE 400
 #define MAX_NUM 10000000.0
 
-int main(){
+int main() {
     srand((unsigned) time(NULL));
-    int size = MAX_SIZE, iterator= 0; // MIN_SIZE + (rand() % MAX_SIZE), iterator = 0;
-    double time_spent = 0.0;
-    double A[size][size] , B[size][size] , C[size][size];
 
-    while(iterator++ < N){
+    int size = MAX_SIZE, iterator = 0; // o usar: MIN_SIZE + (rand() % MAX_SIZE)
+    double time_spent = 0.0;
+    double A[size][size], B[size][size], C[size][size];
+
+    while (iterator++ < N) {
         printf("Running iteration number: %d\n", iterator);
 
         clock_t begin_time = clock();
-        
+
         // Start modifying here
 
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                A[j][i] = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; 
-                B[j][i] = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; 
-                C[j][i] = 0;
+        // Usamos punteros para acceder secuencialmente
+        double *pA = &A[0][0];
+        double *pB = &B[0][0];
+        double *pC = &C[0][0];
+
+        // Inicialización
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                *(pA + i * size + j) = 1 + ((double)rand() / RAND_MAX) * MAX_NUM;
+                *(pB + j * size + i) = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; // transpuesta
+                *(pC + i * size + j) = 0;
             }
         }
-        
-        for(int j = 0; j < size; j++){
-            for(int k = 0; k < size; k++){
-                for(int i = 0; i < size; i++){
-                    C[i][j] += A[i][k] * B[k][j];
+
+        // Multiplicación
+        for (int j = 0; j < size; j++) {
+            for (int k = 0; k < size; k++) {
+                for (int i = 0; i < size; i++) {
+                    *(pC + i * size + j) +=
+                        *(pA + i * size + k) * *(pB + j * size + k);
                 }
             }
         }
 
         // Stop modifying here
-        
+
         clock_t end_time = clock();
         time_spent += (double)(end_time - begin_time) / CLOCKS_PER_SEC;
     }
 
-    printf("Size of matrices: %d \n", size);
-    printf("Running time: %f \n", (time_spent / N));
+    printf("Size of matrices: %d\n", size);
+    printf("Running time: %f\n", (time_spent / N));
 
     return 0;
 }
