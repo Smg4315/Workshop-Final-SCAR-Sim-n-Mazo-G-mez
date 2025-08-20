73ad6671ff7d90589e502a284e0653ab86380c8e
Saimon <smazog@eafit.edu.co>
2025-08-20 11:03:55 -0500

    parallelization using OpenMP implemented, i cant run it in my pc, so im testing it in another one

--- mmm_implementation.c
+++ mmm_implementation.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 #define N 50
 #define MIN_SIZE 399
@@ -12,7 +13,8 @@ int main() {
 
     int size = MAX_SIZE, iterator = 0; // MIN_SIZE + (rand() % MAX_SIZE)
     double time_spent = 0.0;
-    double A[size][size], B[size][size], C[size][size];
+    double A[size][size] __attribute__((aligned(64))), B[size][size] __attribute__((aligned(64))), C[size][size] __attribute__((aligned(64)));
+
 
     while (iterator++ < N) {
         printf("Running iteration number: %d\n", iterator);
@@ -27,15 +29,19 @@ int main() {
         double *pC = &C[0][0];
 
         // Inicialización -> llenamos de valores aleatorios las matrices A y B (usando punteros)
+        #pragma omp parallel for collapse(2)
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                *(pA + i * size + j) = 1 + ((double)rand() / RAND_MAX) * MAX_NUM;
-                *(pB + j * size + i) = 1 + ((double)rand() / RAND_MAX) * MAX_NUM; // usamos la transpuesta de B para facilidad de acceso
+                unsigned int seed = time(NULL) + omp_get_thread_num();
+                *(pA + i * size + j) = 1 + ((double)rand_r(&seed) / RAND_MAX) * MAX_NUM; // Cambiamos la forma en que se accedde
+                *(pB + j * size + i) = 1 + ((double)rand_r(&seed) / RAND_MAX) * MAX_NUM; // al random para optimizar la  paralelización
                 *(pC + i * size + j) = 0;
             }
         }
 
+
         // Multiplicamos teniendo en cuenta el acceso de punteros
+        #pragma omp parallel for
         for (int j = 0; j < size; j++) {
             for (int k = 0; k < size; k++) {
                 for (int i = 0; i < size; i++) {
